03a1f55fb86ab3b002f62c8d77132fbe6bd7430f
Juan Jose Fernandez Garcete <fernandezgarcete@gmail.com>
2020-07-16 09:38:03 -0300

    Adding ViewSet to our API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 
 from profiles_api import serializers
 
@@ -46,3 +46,18 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an object"""
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self, request):
+        """Return a Hello message"""
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello', 'a_viewset': a_viewset})
+
